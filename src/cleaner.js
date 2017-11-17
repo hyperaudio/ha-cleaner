c993bba87cb12868c5d9409f067a5a2d994e6b3b
Laurian Gridinoc <laurian@gridinoc.name>
2017-11-17 12:39:23 +0100

    prettier reformat

--- src/cleaner.js
+++ src/cleaner.js
@@ -1,9 +1,10 @@
-
-$( document ).ready(function() {
-
+$(document).ready(function() {
   var namespace = null;
   if (document.location.hostname.indexOf('hyperaud') > 0) {
-    namespace = document.location.hostname.substring(0, document.location.hostname.indexOf('hyperaud') - 1);
+    namespace = document.location.hostname.substring(
+      0,
+      document.location.hostname.indexOf('hyperaud') - 1
+    );
   }
 
   var prefix = '';
@@ -21,15 +22,15 @@ $( document ).ready(function() {
     org: namespace, // The organisations namespace / sub-domain. EG. 'chattanooga'
     domain: domain,
     protocol: 'https://',
-		bgm: 'media?tag=bgm',
-		whoami: 'auth/whoami/',
-		signin: 'accounts/token',
-		withCredentials: false
+    bgm: 'media?tag=bgm',
+    whoami: 'auth/whoami/',
+    signin: 'accounts/token',
+    withCredentials: false
   });
 
   var API = 'https://' + prefix + 'api.' + domain + '/v1';
 
-  $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+  $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
     if (options.url.indexOf(API) == 0) {
       if (window.localStorage.getItem('token')) {
         jqXHR.setRequestHeader('Authorization', 'Bearer ' + window.localStorage.getItem('token'));
@@ -43,27 +44,30 @@ $( document ).ready(function() {
   var transcriptObject;
   var user;
 
-  var transcriptEl = document.getElementById("source-transcript");
+  var transcriptEl = document.getElementById('source-transcript');
 
   var savingAnim = document.querySelector('#save-button-saving');
 
-  transcriptEl.addEventListener("mouseup", getSelection, false);
-  transcriptEl.addEventListener("touchend", getSelection, false);
+  transcriptEl.addEventListener('mouseup', getSelection, false);
+  transcriptEl.addEventListener('touchend', getSelection, false);
 
   function getSelection() {
     $('#format-para').removeAttr('disabled');
   }
 
-  $("#format-para").click(function() {
-    console.log("--- getting selection ---");
+  $('#format-para').click(function() {
+    console.log('--- getting selection ---');
     var selection = transcriptObject.getSelection();
 
     //console.dir($("a[data-m='"+selection.start+"']"));
 
-    var $start = $("a[data-m='"+selection.start+"']");
-    var $end = $("a[data-m='"+selection.end+"']");
+    var $start = $("a[data-m='" + selection.start + "']");
+    var $end = $("a[data-m='" + selection.end + "']");
 
-    $start.nextUntil($end.next()).andSelf().wrapAll("<p/>");
+    $start
+      .nextUntil($end.next())
+      .andSelf()
+      .wrapAll('<p/>');
 
     //console.log("prev length :"+$start.prev().length);
     /*if ($start.prev().length == 0) {
@@ -85,33 +89,34 @@ $( document ).ready(function() {
     /*$("<br>").insertBefore($start);
     $("<br>").insertAfter($end);*/
 
-
     console.log(selection.text);
     console.log(selection.start);
     console.log(selection.end);
   });
 
-
   function whoami(callback) {
-    $.ajax(API+'/auth/whoami/' + window.localStorage.getItem('token'), {
-      type: "GET",
-      contentType: "application/json; charset=utf-8",
+    $.ajax(API + '/auth/whoami/' + window.localStorage.getItem('token'), {
+      type: 'GET',
+      contentType: 'application/json; charset=utf-8',
       success: function(whoami) {
         if (whoami.user) {
-            // logged in
-            //alert('logged in');
-            user = whoami.user;
+          // logged in
+          //alert('logged in');
+          user = whoami.user;
 
-            var event = new CustomEvent("ga", {"detail":{"origin":"HA-Cleaner","type":"XHR","action":"User logged in (whoami)"}});
-            document.dispatchEvent(event);
-
+          var event = new CustomEvent('ga', {
+            detail: { origin: 'HA-Cleaner', type: 'XHR', action: 'User logged in (whoami)' }
+          });
+          document.dispatchEvent(event);
         } else {
-            // not logged in
-            //alert('NOT logged in');
-            user = null;
+          // not logged in
+          //alert('NOT logged in');
+          user = null;
 
-            var event = new CustomEvent("ga", {"detail":{"origin":"HA-Cleaner","type":"XHR","action":"User NOT logged in (whoami)"}});
-            document.dispatchEvent(event);
+          var event = new CustomEvent('ga', {
+            detail: { origin: 'HA-Cleaner', type: 'XHR', action: 'User NOT logged in (whoami)' }
+          });
+          document.dispatchEvent(event);
         }
         if (callback) callback();
       },
@@ -124,130 +129,124 @@ $( document ).ready(function() {
 
   /*----------------*/
 
-
   var selectWordTime = 0;
 
   var player = HA.Player({
-    target: "#source-video",
+    target: '#source-video',
     gui: {
       navigation: false,
       fullscreen: false
     }
   });
 
-
-  var transcriptEl = document.getElementById("source-transcript");
+  var transcriptEl = document.getElementById('source-transcript');
 
   transcriptEl.addEventListener(HA.event.ready, setListeners, false);
 
   if (transcriptID) {
     transcriptObject = HA.Transcript({
-        target: "#source-transcript",
-        id: transcriptID,
-        player: player
+      target: '#source-transcript',
+      id: transcriptID,
+      player: player
     });
     console.dir(transcriptObject);
   }
 
   function setListeners() {
-
-    var words = document.getElementsByTagName("a");
-    var selectedWord = document.getElementById("selected-word");
-    var selectedWordTime = document.getElementById("selected-word-time");
-    var tweakTime = document.getElementById("tweak-time");
+    var words = document.getElementsByTagName('a');
+    var selectedWord = document.getElementById('selected-word');
+    var selectedWordTime = document.getElementById('selected-word-time');
+    var tweakTime = document.getElementById('tweak-time');
     var timeChange;
 
     tweakTime.value = 0;
-    selectedWord.value = "";
-    selectedWordTime.value = "";
+    selectedWord.value = '';
+    selectedWordTime.value = '';
 
     for (var i = 0; i < words.length; i++) {
       words[i].onclick = function() {
         //console.dir(this.getAttribute("data-m"));
-        selectWordTime = this.getAttribute("data-m");
-        selectedWord.value = this.innerHTML;//.replace(" ","");
+        selectWordTime = this.getAttribute('data-m');
+        selectedWord.value = this.innerHTML; //.replace(" ","");
         selectedWordTime.value = selectWordTime;
         tweakTime.value = 0;
-      }
+      };
     }
 
     selectedWord.oninput = function() {
-      var theWord = document.querySelectorAll("a[data-m='"+selectWordTime+"']");
+      var theWord = document.querySelectorAll("a[data-m='" + selectWordTime + "']");
       theWord[0].innerHTML = selectedWord.value;
-    }
+    };
 
     selectedWordTime.oninput = function() {
-      var theWord = document.querySelectorAll("a[data-m='"+selectWordTime+"']");
+      var theWord = document.querySelectorAll("a[data-m='" + selectWordTime + "']");
       //console.log(ht.selectWordTime);
-      theWord[0].setAttribute("data-m",(selectedWordTime.value));
+      theWord[0].setAttribute('data-m', selectedWordTime.value);
       selectWordTime = selectedWordTime.value;
 
-      console.log("swt on "+selectedWordTime.value);
-    }
+      console.log('swt on ' + selectedWordTime.value);
+    };
 
     selectedWordTime.onkeyup = function(e) {
       if (e.keyCode == 13) {
-        player.play(parseInt(selectWordTime)/1000);
+        player.play(parseInt(selectWordTime) / 1000);
         console.log(selectWordTime);
       }
-    }
+    };
 
     $('#tweak-time').on('input', function() {
       timeChange = parseInt(this.value);
-      player.play((parseInt(selectWordTime)+timeChange)/1000);
+      player.play((parseInt(selectWordTime) + timeChange) / 1000);
       //selectedWordTime.value = parseInt(selectWordTime)+timeChange;
-      console.log("timeChange on "+timeChange);
-      console.log("selectWordTime on "+selectWordTime);
+      console.log('timeChange on ' + timeChange);
+      console.log('selectWordTime on ' + selectWordTime);
       //return false;
     });
 
     //tweakTime.onkeyup = setWordTime;
     //tweakTime.onmouseup = setWordTime;
 
-    $('#tweak-time').mouseup(function(e){
+    $('#tweak-time').mouseup(function(e) {
       setWordTime();
       /*e.preventDefault();*/
       /*return false;*/
     });
 
-    $('#tweak-time').keyup(function(e){
+    $('#tweak-time').keyup(function(e) {
       setWordTime();
       /*e.preventDefault();*/
       /*return false;*/
     });
 
     function setWordTime() {
-      var theWord = document.querySelectorAll("a[data-m='"+selectWordTime+"']");
-      selectWordTime = parseInt(selectWordTime)+timeChange;
-      theWord[0].setAttribute("data-m",selectWordTime);
+      var theWord = document.querySelectorAll("a[data-m='" + selectWordTime + "']");
+      selectWordTime = parseInt(selectWordTime) + timeChange;
+      theWord[0].setAttribute('data-m', selectWordTime);
       selectedWordTime.value = selectWordTime;
-      console.log("timeChange up "+timeChange);
-      console.log("selectWordTime up "+selectWordTime);
+      console.log('timeChange up ' + timeChange);
+      console.log('selectWordTime up ' + selectWordTime);
       timeChange = 0;
     }
 
     //setInterval(function(){console.log("tc="+timeChange);$('#selected-word-time').val(parseInt(selectWordTime)+parseInt(timeChange))},500);
-
   }
 
-
   function updateTranscript(user) {
+    var $copy = $('#source-transcript').clone();
+    $copy.find('a').removeClass('transcript-grey');
+    $copy.find('a').removeClass('selected');
 
-    var $copy = $("#source-transcript").clone();
-    $copy.find("a").removeClass("transcript-grey");
-    $copy.find("a").removeClass("selected");
-
-    console.log("user:"+user);
-    console.log("owner:"+HA.api.transcript.owner);
+    console.log('user:' + user);
+    console.log('owner:' + HA.api.transcript.owner);
     console.dir(HA.api.transcript);
 
     savingAnim.style.display = 'block';
 
     if (user != HA.api.transcript.owner) {
-      $.ajax( API + '/transcripts/', {
-        type: "POST",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
+      $.ajax(API + '/transcripts/', {
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
         data: JSON.stringify({
           label: HA.api.transcript.label,
           type: 'html',
@@ -275,10 +274,10 @@ $( document ).ready(function() {
         crossDomain: true
       });
     } else {
-      $.ajax( API + '/transcripts/' + transcriptID, {
-        type: "PUT",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
+      $.ajax(API + '/transcripts/' + transcriptID, {
+        type: 'PUT',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
         data: JSON.stringify({
           _id: transcriptID,
           label: HA.api.transcript.label,
@@ -307,7 +306,6 @@ $( document ).ready(function() {
         crossDomain: true
       });
     }
-
   }
 
   function save() {
@@ -317,7 +315,7 @@ $( document ).ready(function() {
         if (transcriptObject) {
           updateTranscript(user);
         } else {
-          alert('error no transcriptObject')
+          alert('error no transcriptObject');
         }
         //
       } else {
